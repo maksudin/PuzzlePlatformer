c1e3d84c40ffee5f28b3c0749a88ee1b8a537c1f
Maksim Iljushkin <m.iljushkin2@yandex.ru>
2022-02-17 11:35:48 +0300

    Половину голов сделал

---
+++ Assets/Scripts/Creatures/Mobs/BaseShootingTrap.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using PixelCrew.Components.ColliderBased;
+using PixelCrew.Components.GoBased;
+using PixelCrew.Utils;
+using UnityEngine;
+
+namespace PixelCrew.Creatures.Mobs
+{
+    public class BaseShootingTrap : MonoBehaviour
+    {
+        [SerializeField] protected LayerCheck Vision;
+
+        [Header("Range")]
+        [SerializeField] protected SpawnComponent ProjectileAttack;
+        [SerializeField] protected Cooldown RangeCooldown;
+
+        protected Animator Animator;
+        protected static readonly int RangeKey = Animator.StringToHash("range_attack");
+
+        protected virtual void Awake()
+        {
+            Animator = GetComponent<Animator>();
+        }
+
+        protected virtual void Update()
+        {
+            if (Vision.IsTouchingLayer)
+            {
+                if (RangeCooldown.IsReady)
+                {
+                    RangeAttack();
+                }
+            }
+        }
+
+        protected virtual void RangeAttack()
+        {
+            RangeCooldown.Reset();
+            Animator.SetTrigger(RangeKey);
+        }
+
+        public void OnRangeAttack()
+        {
+            ProjectileAttack.Spawn();
+        }
+    }
+}
---
+++ Assets/Scripts/Creatures/Mobs/ShootingMeleeTrapAI.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using PixelCrew.Components.ColliderBased;
+using PixelCrew.Utils;
+using UnityEngine;
+
+namespace PixelCrew.Creatures.Mobs
+{
+    public class ShootingMeleeTrapAI : BaseShootingTrap
+    {
+        [Header("Melee")]
+        [SerializeField] private CheckCircleOverlap _meleeAttack;
+        [SerializeField] private LayerCheck _meleeCanAttack;
+        [SerializeField] private Cooldown _meleeCooldown;
+
+        private static readonly int MeleeKey = Animator.StringToHash("melee_attack");
+
+        protected override void Update()
+        {
+            base.Update();
+
+            if (Vision.IsTouchingLayer)
+            {
+                if (_meleeCanAttack.IsTouchingLayer)
+                {
+                    if (_meleeCooldown.IsReady)
+                        MeleeAttack();
+                    return;
+                }
+            }
+        }
+
+        protected override void RangeAttack()
+        {
+            base.RangeAttack();
+            Animator.SetTrigger(RangeKey);
+        }
+
+        private void MeleeAttack()
+        {
+            _meleeCooldown.Reset();
+            Animator.SetTrigger(MeleeKey);
+        }
+
+        public void OnMeleeAttack()
+        {
+            _meleeAttack.Check();
+        }
+
+    }
+}
