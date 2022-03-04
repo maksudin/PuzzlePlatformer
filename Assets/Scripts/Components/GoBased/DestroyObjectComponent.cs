74250eb888395db87d6930d64516cebb8f65a934
Maksim Iljushkin <m.iljushkin2@yandex.ru>
2022-03-04 14:09:41 +0300

    Пофиксил баг не полного проигрывания звуков.
    Теперь Destroy вызывается после полного проигрывания AudioSource

--- Assets/Scripts/Components/GoBased/DestroyObjectComponent.cs
+++ Assets/Scripts/Components/GoBased/DestroyObjectComponent.cs
@@ -7,11 +7,32 @@ namespace PixelCrew.Components.GoBased
     public class DestroyObjectComponent : MonoBehaviour
     {
         [SerializeField] private GameObject _objectToDestroy;
+        private AudioSource _audioSource;
+        private bool _hasAudio = false;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+            _hasAudio = _audioSource? true : false;
+        }
+
         public void DestroyObject()
+        {
+            if (_hasAudio)
+            {
+                Invoke(nameof(DestroyObjectWithAudio), _audioSource.clip.length);
+            }
+            else
+            {
+                Destroy(_objectToDestroy);
+            }
+        }
+
+        private void DestroyObjectWithAudio()
         {
             Destroy(_objectToDestroy);
         }
-        
+
     }
 
 }
