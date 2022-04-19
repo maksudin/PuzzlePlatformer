89ad1443b980f9e1775339032964a46087ed43ba
Maksim Iljushkin <m.iljushkin2@yandex.ru>
2022-04-19 16:07:21 +0300

    Начал делать parallax

---
+++ Assets/PixelCrew/Effects/InfiniteBackground.cs
@@ -0,0 +1,37 @@
+﻿using PixelCrew.Utils;
+using UnityEngine;
+
+namespace PixelCrew.Effects
+{
+    public class InfiniteBackground : MonoBehaviour
+    {
+        [SerializeField] private Camera _camera;
+        [SerializeField] private Transform _container;
+
+        private Bounds _containerBounds;
+        private Bounds _allBounds;
+
+        private Vector3 _boundsToTransformDelta;
+        private Vector3 _containerDelta;
+
+        private void Start()
+        {
+            var sprites = _container.GetComponentsInChildren<SpriteRenderer>();
+            foreach (var sprite in sprites)
+            {
+                _containerBounds.Encapsulate(sprite.bounds);
+            }
+
+            _allBounds = _containerBounds;
+
+            _boundsToTransformDelta = transform.position - _allBounds.center;
+            _containerDelta = _container.position - _allBounds.center;
+        }
+
+
+        private void OnDrawGizmosSelected()
+        {
+            GizmosUtils.DrawBounds(_allBounds, Color.magenta);
+        }
+    }
+}
---
+++ Assets/PixelCrew/Utils/GizmosUtils.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace PixelCrew.Utils
+{
+    public class GizmosUtils
+    {
+        public static void DrawBounds(Bounds bounds, Color color)
+        {
+            var prevColor = Gizmos.color;
+            Gizmos.color = color;
+            Gizmos.DrawLine(bounds.min, new Vector3(bounds.min.x, bounds.max.y)); // лево
+            Gizmos.DrawLine(bounds.max, new Vector3(bounds.max.x, bounds.min.y)); // право
+            Gizmos.DrawLine(new Vector3(bounds.min.x, bounds.max.y), bounds.max); // верх
+            Gizmos.DrawLine(new Vector3(bounds.max.x, bounds.min.y), bounds.min); // низ
+
+            Gizmos.color = prevColor;
+        }
+    }
+}
