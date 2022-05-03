311af8278286eea1bce41991a2531174b4a65a6e
Maksim Iljushkin <m.iljushkin2@yandex.ru>
2022-05-03 11:17:48 +0300

    Сделал иконку активного перка в HUD.

---
+++ Assets/PixelCrew/Components/UI/HUD/PerksUsedWidget.cs
@@ -0,0 +1,28 @@
+﻿using PixelCrew.Model;
+using PixelCrew.Model.Definitions;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.PixelCrew.Components.UI.HUD
+{
+    public class PerksUsedWidget : MonoBehaviour
+    {
+        [SerializeField] private GameObject _iconObject;
+        private GameSession _session;
+
+        private void Start()
+        {
+            _session = FindObjectOfType<GameSession>();
+            _session.PerksModel.Subscribe(UpdateUsedPerk);
+        }
+
+        private void UpdateUsedPerk()
+        {
+            var perkId = _session.PerksModel.Used;
+            if (perkId == "") return;
+
+            _iconObject.SetActive(true);
+            _iconObject.GetComponent<Image>().sprite = DefsFacade.I.Perks.Get(perkId).Icon;
+        }
+    }
+}
