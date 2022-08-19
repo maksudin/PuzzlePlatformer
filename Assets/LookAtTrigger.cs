966cb8bd0aeee70cb561cdc39540043b965b18d3
Maksim Iljushkin <m.iljushkin2@yandex.ru>
2022-08-19 15:05:08 +0300

    Math assignment

---
+++ Assets/LookAtTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class LookAtTrigger : MonoBehaviour
+{
+    [SerializeField] private Transform _playertr;
+    [SerializeField] private float dotPB;
+    [Range(0f, 1f)]
+    [SerializeField] private float canSeeTreshold;
+    [SerializeField] private bool playerLookingAtB;
+
+    private void OnDrawGizmos()
+    {
+        var center = transform.position;
+        var playerPos = _playertr.position;
+        var playerLookDir = _playertr.right;
+        var playetTriggerDir = (center - playerPos).normalized;
+
+        dotPB = Vector2.Dot(playetTriggerDir, playerLookDir);
+        playerLookingAtB = dotPB >= canSeeTreshold;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(playerPos, playerPos + playerLookDir);
+
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(playerPos, playerPos + playetTriggerDir);
+    }
+}
---
+++ Assets/MyTransform.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MyTransform : MonoBehaviour
+{
+    [SerializeField] private Transform point;
+    [SerializeField] private Vector2 coords;
+
+    private void OnDrawGizmos()
+    {
+        Vector2 pointPos = point.position;
+        Vector2 vectorX = transform.position + transform.right;
+        Vector2 vectorY = transform.position + transform.up;
+
+        var projX = Vector2.Dot(vectorX - pointPos, vectorX);
+        var projY = Vector2.Dot(point.position, vectorY);
+        coords = new Vector2(projX, projY);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, vectorX);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, vectorY);
+
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(transform.position, point.position);
+    }
+}
