54e4ad79c58314e4eb4d6dbe8faeb6bebe31bc78
Maksim Iljushkin <m.iljushkin2@yandex.ru>
2022-10-10 19:33:40 +0300

    Удалил лишние сцены, начал делать разделение уровня на скрины.

---
+++ Assets/PixelCrew/LevelFrame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class LevelFrame : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+
+    private void OnDrawGizmos()
+    {
+        if (_camera == null) return;
+
+        float height = 2f * _camera.orthographicSize;
+        float width = height * _camera.aspect;
+
+        Handles.color = Color.red;
+        Handles.DrawWireCube(transform.position, new Vector3(width, height, 1));
+    }
+
+}
