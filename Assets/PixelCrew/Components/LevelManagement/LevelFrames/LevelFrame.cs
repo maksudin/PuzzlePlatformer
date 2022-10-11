2942ae3276515fb35f777df58538c302a548c395
Maksim Iljushkin <m.iljushkin2@yandex.ru>
2022-10-11 14:11:11 +0300

    Простая реализация деления на фреймы уровня готова.

---
+++ Assets/PixelCrew/Components/LevelManagement/LevelFrames/LevelFrame.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.PixelCrew.Components.LevelManagement.LevelFrames
+{
+    [Serializable]
+    public class LevelFrame : MonoBehaviour
+    {
+        [SerializeField] private Camera _camera;
+        [SerializeField] private float FrameWidthMultiplier = 1f;
+        [SerializeField] private float FrameHeightMultiplier = 1f;
+
+        [NonSerialized] public float Width;
+        [NonSerialized] public float Height;
+        [NonSerialized] public bool IsActive = false;
+
+        private void Start()
+        {
+            if (_camera == null) return;
+            Height = 2f * _camera.orthographicSize * FrameHeightMultiplier;
+            Width = Height * _camera.aspect * FrameWidthMultiplier;
+        }
+
+
+        private void OnDrawGizmos()
+        {
+            if (_camera == null) return;
+
+            Height = 2f * _camera.orthographicSize * FrameHeightMultiplier;
+            Width = Height * _camera.aspect * FrameWidthMultiplier;
+
+            Handles.color = IsActive ? Color.green : Color.red;
+            Handles.DrawWireCube(transform.position, new Vector3(Width, Height, 1));
+        }
+    }
+}
---
+++ Assets/PixelCrew/Components/LevelManagement/LevelFrames/LevelFramesManager.cs
@@ -0,0 +1,49 @@
+﻿using PixelCrew.Creatures.Hero;
+using UnityEngine;
+
+namespace Assets.PixelCrew.Components.LevelManagement.LevelFrames
+{
+    public class LevelFramesManager : MonoBehaviour
+    {
+        [SerializeField] private LevelFrame[] _levelFrames;
+        private LevelFrame _activeFrame;
+        private Transform _heroTransform;
+        private Camera _camera;
+
+        private void Start()
+        {
+            _heroTransform = FindObjectOfType<Hero>().GetComponent<Transform>();
+            _camera = FindObjectOfType<Camera>();
+        }
+
+        private void Update()
+        {
+            ActivateHeroLevelFrame(_levelFrames);
+        }
+
+        private void ActivateHeroLevelFrame(LevelFrame[] levelFrames)
+        {
+            foreach (var frame in levelFrames)
+            {
+                var heroFramePos = frame.transform.InverseTransformPoint(_heroTransform.position);
+                if ( Mathf.Abs(heroFramePos.x) <= frame.Width / 2 && Mathf.Abs(heroFramePos.y) <= frame.Height / 2 )
+                {
+                    frame.IsActive = true;
+                    _activeFrame = frame;
+                    MoveCameraToActiveFrame();
+                }
+                else
+                    frame.IsActive = false;
+            }
+        }
+
+        private void MoveCameraToActiveFrame()
+        {
+            _camera.transform.position = new Vector3(
+                _activeFrame.transform.position.x,
+                _activeFrame.transform.position.y,
+                _camera.transform.position.z
+            );
+        }
+    }
+}
